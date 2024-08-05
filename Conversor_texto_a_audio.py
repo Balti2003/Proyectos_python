647c7b0db0364b700582c7730432630df0efc123
Baltasar Lomello <baltasarlomello@live.com>
2024-08-05 14:32:41 -0300

    Siguiendo el texto a audio

--- Conversor_texto_a_audio.py
+++ Conversor_texto_a_audio.py
@@ -3,3 +3,8 @@ import os
 
 text = "Hola me llamo Baltasar y vos?"
 language = "es-us"
+speech = gTTs(text = text, lang = language, slow = False)
+
+speech.save("texto.mp3")
+
+os.system("start texto.mp3")
